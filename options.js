96c5f64de3d9af701c03cb40020e765e28f2673f
mishikawa-Horornis <Massagoegoe-8192>
2025-09-15 06:31:31 +0900

    committed

--- options.js
+++ options.js
@@ -1,19 +1,23 @@
+// SPDX-License-Identifier: MIT
 (async function(){
   const $ = (id) => document.getElementById(id);
+  // options.js 保存時に追加
+  await browser.storage.local.set({
+    vtApiKey: $("#vtApiKey").value.trim(),
+    gsbApiKey: $("#gsbApiKey").value.trim(),
+    ptAppKey:  $("#ptAppKey").value.trim(),
+    toAntiPhishing: $("#toAntiPhishing").value.trim(),
+    toDekyo: $("#toDekyo").value.trim(),
+    attachEml: $("#attachEml").checked
+  });
+  $("vtApiKey").value = vtApiKey;
+  $("gsbApiKey").value = gsbApiKey;
+  $("ptAppKey").value = ptAppKey;
+  $("toAntiPhishing").value = toAntiPhishing;
+  $("toDekyo").value = toDekyo;
+  $("attachEml").checked = attachEml;
 
-  // ページロード時にストレージから値を取得してフォームに反映
-  const saved = await browser.storage.local.get([
-    "vtApiKey", "gsbApiKey", "ptAppKey", "toAntiPhishing", "toDekyo", "attachEml"
-  ]);
-
-  $("vtApiKey").value = saved.vtApiKey || "";
-  $("gsbApiKey").value = saved.gsbApiKey || "";
-  $("ptAppKey").value = saved.ptAppKey || "";
-  $("toAntiPhishing").value = saved.toAntiPhishing || "info@antiphishing.jp";
-  $("toDekyo").value = saved.toDekyo || "report@dekyo.or.jp";
-  $("attachEml").checked = saved.attachEml ?? true;
-
-  // 保存ボタン押下時に値を保存
+  
   $("save").addEventListener("click", async () => {
     await browser.storage.local.set({
       vtApiKey: $("vtApiKey").value.trim(),
